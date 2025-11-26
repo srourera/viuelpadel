129cbff5f706db7ecf1ce17d475de32f92908dca
srourera <sergirourerallaurado@gmail.com>
2025-11-26 06:48:06 +0100

    First commit also

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import { RouterView } from "vue-router";
+export default {
+  name: "App",
+  components: {
+    RouterView,
+  },
+};
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+export default {
+  name: "HomeView",
+};
+</script>
+
+<template>
+  <div>HomeView</div>
+</template>
+
+<style scoped></style>
